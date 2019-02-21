7c23499e3e81c7087e2e7c4dbf05ecf94cb35f48
LapoFrati <fratilapo@gmail.com>
2019-02-20 23:09:43 -0500

    Tweak speed of threshold visualization

--- _projects/Spikes/p5/threshold/sketch.js
+++ _projects/Spikes/p5/threshold/sketch.js
@@ -23,6 +23,13 @@ function mousePressed() {
 
 function setup() {
     createCanvas(250, 220);
+
+    // GENERATE COLORS FIRST, THEY ARE USED INSIDE NODES
+    spikeColor = color(255, 0, 50);
+    restColor = color(0, 85, 225);
+    impulseColor = color(255);
+    backgroundColor = color(50);
+
     from = new Node(createVector(width / 2 - 90, height / 2), 0);
     to = new Node(createVector(width / 2 + 5, height / 2 + 10), 1);
     nodes.push(from);
@@ -30,13 +37,9 @@ function setup() {
     link = new Link(from, to);
     links.push(link);
     from.out.push(link);
-    spikeColor = color(255, 0, 50);
-    restColor = color(0, 85, 225);
-    impulseColor = color(255);
-    backgroundColor = color(50);
 
     origin = createVector(width / 2 + 30, height / 2);
-    x = new Axis(-5, 100, 'x');
+    x = new Axis(-5, 70, 'x');
     y = new Axis(-5, 100, 'y');
     activation = new Activation();
     to.out.push(activation);
@@ -74,7 +77,6 @@ function draw() {
     }
     for (let node of nodes) {
         node.show();
-        node.update();
     }
 
     push(); // -------------------
@@ -85,6 +87,10 @@ function draw() {
 
     x.show();
     y.show();
+    fill(255);
+    let side = 2;
+    let height = 99;
+    triangle(-side, -height, side, -height, 0, -side * 2 - height);
     pop(); // --------------------
 }
 
@@ -95,56 +101,8 @@ function arrowhead(from, to, base, height, distance) {
     translate(to.pos.x, to.pos.y); // move the origin to the target the arrow is pointing to
     rotate(rot); // rotate to align the tip
     noStroke(); // strong independent arrows need no border
-    fill(100); // a bit of transparency, we want to see them if they overlap
     triangle(-distance / 2 - 1, 0, -distance / 2 - height, -base, -distance / 2 - height, +base);
     pop();
-}
-
-class VanishingLine {
-    constructor(from, to) {
-        this.from = from; // Node
-        this.to = to; // Vector!!!
-        this.length = dist(from.pos.x, from.pos.y, to.x, to.y);
-        this.impulses = [];
-        this.speed = 2;
-    }
-
-    excite() {
-        this.impulses.push(0);
-    }
-
-    show() {
-        for (let i = 0; i <= 1; i += 0.01) {
-            let new_pos = p5.Vector.lerp(this.from.pos, this.to, i);
-            noStroke();
-            if (this.impulses.length > 0) {
-                fill(lerpColor(color(255, 0, 0), backgroundColor, i ** 2));
-            } else {
-                fill(lerpColor(color(255), backgroundColor, i ** 2));
-            }
-            ellipse(new_pos.x, new_pos.y, 1, 1);
-        }
-
-        for (let impulse of this.impulses) {
-            let tmp = p5.Vector.lerp(this.from.pos, this.to, impulse / this.length);
-            let clr = lerpColor(color(255), backgroundColor, impulse / this.length);
-            noStroke();
-            fill(clr);
-            ellipse(tmp.x, tmp.y, 5, 5);
-        }
-    }
-
-    update() {
-        let new_impulses = [];
-
-        for (let impulse of this.impulses) {
-            let new_impulse = impulse + this.speed;
-            if (new_impulse < this.length) {
-                new_impulses.push(new_impulse);
-            }
-        }
-        this.impulses = new_impulses;
-    }
 }
 
 class Activation {
@@ -153,10 +111,11 @@ class Activation {
         this.level = 0;
         this.dx = 0.05;
         this.magnification = 5;
-        this.threshold = 5;
+        this.threshold = 3;
         this.dampening = 0;
-        this.decay = 0.01;
+        this.decay = 0.03;
         this.out = [];
+        this.max_dampening = 14;
     }
     excite() {
         this.activation += 2;
@@ -165,7 +124,7 @@ class Activation {
     update() {
         // let the bar grow until activation
         if (this.level < this.activation) {
-            this.level += this.dx * 2;
+            this.level += this.dx * 4;
         }
 
         // the bar has reached the activation and is above the threshold -> fire
@@ -173,9 +132,11 @@ class Activation {
             for (let out of this.out) {
                 out.excite();
             }
-            this.activation -= this.threshold;
+            this.activation -= this.threshold + this.dampening;
             this.level = 0;
-            this.dampening += 1;
+            if (this.dampening < this.max_dampening) {
+                this.dampening += 2;
+            }
         }
 
         // the bar has reached the activation and is above the threshold -> fire
@@ -198,21 +159,29 @@ class Activation {
     }
     show() {
         stroke(100);
+        let levelColor = lerpColor(
+            restColor,
+            spikeColor,
+            this.level / (this.threshold + this.dampening)
+        );
+
+        to.color = levelColor;
 
         //Activation line for debugging
         //line(0, -this.activation * this.magnification, 40, -this.activation * this.magnification);
 
-        // Activation bar
-        fill(lerpColor(restColor, spikeColor, this.level / (this.threshold + this.dampening)));
+        // Level bar
+        fill(levelColor);
         noStroke();
         rect(20, 0, 20, -this.level * this.magnification);
+
         // Threshold
         stroke(100);
-        strokeWeight(2);
+        strokeWeight(1);
         line(
             0,
             -(this.threshold + this.dampening) * this.magnification,
-            40,
+            50,
             -(this.threshold + this.dampening) * this.magnification
         );
 
@@ -250,47 +219,19 @@ class Node {
         this.id = id;
         this.out = [];
         this.in = [];
-        this.excited = false; // used to draw the spike train
-        this.activation = 0;
-        this.threshold = 5;
-        this.dampening = 0;
-        this.decay = 0.01;
+        this.excited = false;
+        this.color = restColor;
     }
     excite() {
         for (let edge of this.out) {
             edge.excite();
         }
     }
-
-    update() {
-        if (this.activation > 0) {
-            this.activation -= this.decay * 2;
-        }
-
-        if (this.activation < 0) {
-            this.activation = 0;
-        }
-
-        if (this.dampening > 0) {
-            this.dampening -= this.decay / 2; // slow dampening recover to let the acitivity die out
-        }
-    }
-
     show() {
         stroke(50);
         strokeWeight(1);
-
-        let size = node_size + this.activation * 4;
-        if (size >= 0) {
-            fill(
-                lerpColor(
-                    restColor,
-                    spikeColor,
-                    this.activation / (this.threshold + this.dampening)
-                )
-            );
-            ellipse(this.pos.x, this.pos.y, size, size);
-        }
+        fill(this.color);
+        ellipse(this.pos.x, this.pos.y, node_size, node_size);
     }
 }
 
@@ -311,11 +252,13 @@ class Link {
         strokeWeight(1);
         if (this.impulses.length == 0) {
             stroke(100); // default is greyish
+            fill(100);
         } else {
-            stroke(255, 50, 50); // excited is reddish
+            stroke(110, 50, 50); // excited is reddish
+            fill(110, 50, 50);
         }
         line(this.from.pos.x, this.from.pos.y, this.to.pos.x, this.to.pos.y);
-        arrowhead(this.from, this.to, 4, 10, node_size);
+        arrowhead(this.from, this.to, 4, 10, node_size - 3);
 
         for (let impulse of this.impulses) {
             let tmp = p5.Vector.lerp(this.from.pos, this.to.pos, impulse / this.length);
