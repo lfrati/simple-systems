a1d69059fe81f769100a15515ccbf6e3989a88b7
LapoFrati <fratilapo@gmail.com>
2019-02-23 20:08:50 -0500

    Add background sketch for main page.

---
+++ home_background.js
@@ -0,0 +1,174 @@
+let flock;
+let numBoids = 50;
+let aim;
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', -1);
+
+    flock = new Flock();
+    // Add an initial set of boids into the system
+    for (let i = 0; i < numBoids; i++) {
+        let b = new Boid(width / 2, height / 2);
+        flock.addBoid(b);
+    }
+}
+
+function draw() {
+    background(255);
+    aim = createVector(mouseX, mouseY);
+    flock.run();
+}
+
+// Add a new boid into the System
+function mouseDragged() {
+    flock.addBoid(new Boid(mouseX, mouseY));
+}
+
+// Flock object
+// Does very little, simply manages the array of all the boids
+
+class Flock {
+    constructor() {
+        // An array for all the boids
+        this.boids = []; // Initialize the array
+    }
+
+    run() {
+        for (let i = 0; i < this.boids.length; i++) {
+            this.boids[i].run(this.boids); // Passing the entire list of boids to each boid individually
+        }
+    }
+    addBoid(b) {
+        this.boids.push(b);
+    }
+}
+
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Boid class
+// Methods for Separation, Cohesion, Alignment added
+
+class Boid {
+    constructor(x, y) {
+        this.acceleration = createVector(0, 0);
+        this.velocity = createVector(random(-1, 1), random(-1, 1));
+        this.position = createVector(x, y);
+        this.r = 3.0;
+        this.maxspeed = 3; // Maximum speed
+        this.maxforce = 0.05; // Maximum steering force
+    }
+
+    run(boids) {
+        this.flock(boids);
+        this.update();
+        this.borders();
+        this.render();
+    }
+
+    applyForce(force) {
+        // We could add mass here if we want A = F / M
+        this.acceleration.add(force);
+    }
+
+    // We accumulate a new acceleration each time based on three rules
+    flock(boids) {
+        let chase = this.seek(aim);
+        let sep = this.separate(boids);
+        // Arbitrarily weight these forces
+        chase.mult(1.5);
+        sep.mult(1.0);
+        // Add the force vectors to acceleration
+        this.applyForce(chase);
+        this.applyForce(sep);
+    }
+    // Separation
+    // Method checks for nearby boids and steers away
+    separate(boids) {
+        let desiredseparation = 25.0;
+        let steer = createVector(0, 0);
+        let count = 0;
+        // For every boid in the system, check if it's too close
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position, boids[i].position);
+            // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+            if (d > 0 && d < desiredseparation) {
+                // Calculate vector pointing away from neighbor
+                let diff = p5.Vector.sub(this.position, boids[i].position);
+                diff.normalize();
+                diff.div(d); // Weight by distance
+                steer.add(diff);
+                count++; // Keep track of how many
+            }
+        }
+        // Average -- divide by how many
+        if (count > 0) {
+            steer.div(count);
+        }
+
+        // As long as the vector is greater than 0
+        if (steer.mag() > 0) {
+            // Implement Reynolds: Steering = Desired - Velocity
+            steer.normalize();
+            steer.mult(this.maxspeed);
+            steer.sub(this.velocity);
+            steer.limit(this.maxforce);
+        }
+        return steer;
+    }
+
+    // Method to update location
+    update() {
+        // Update velocity
+        this.velocity.add(this.acceleration);
+        // Limit speed
+        this.velocity.limit(this.maxspeed);
+        this.position.add(this.velocity);
+        // Reset accelertion to 0 each cycle
+        this.acceleration.mult(0);
+    }
+
+    // A method that calculates and applies a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+    seek(target) {
+        let desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
+        // Normalize desired and scale to maximum speed
+        desired.normalize();
+        desired.mult(this.maxspeed);
+        // Steering = Desired minus Velocity
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce); // Limit to maximum steering force
+        return steer;
+    }
+
+    render() {
+        // Draw a triangle rotated in the direction of velocity
+        let theta = this.velocity.heading() + radians(90);
+        fill(127);
+        noStroke();
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+        vertex(0, -this.r * 2);
+        vertex(-this.r, this.r * 2);
+        vertex(this.r, this.r * 2);
+        endShape(CLOSE);
+        pop();
+    }
+
+    // Wraparound
+    borders() {
+        if (this.position.x < -this.r) this.position.x = width + this.r;
+        if (this.position.y < -this.r) this.position.y = height + this.r;
+        if (this.position.x > width + this.r) this.position.x = -this.r;
+        if (this.position.y > height + this.r) this.position.y = -this.r;
+    }
+}
