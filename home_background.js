56e34d411222514d5bd65498b4792320707d6570
LapoFrati <fratilapo@gmail.com>
2019-02-24 01:39:33 -0500

    Add snake predator and fancy deaths

--- home_background.js
+++ home_background.js
@@ -1,6 +1,12 @@
 let flock;
-let numBoids = 50;
+let numBoids = 20;
 let aim;
+
+let segNum = 5;
+let segLength = 5;
+
+let splatters = [];
+let serpent;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -14,15 +20,26 @@ function setup() {
     flock = new Flock();
     // Add an initial set of boids into the system
     for (let i = 0; i < numBoids; i++) {
-        let b = new Boid(0, 0);
+        let b = new Boid(width / 2, height / 2);
         flock.addBoid(b);
     }
+
+    startPos = createVector(0, 0);
+    serpent = new Serpent();
 }
 
 function draw() {
     background(255);
     aim = createVector(mouseX, mouseY);
     flock.run();
+
+    serpent.chase(flock);
+    serpent.show();
+
+    for (let particle of splatters) {
+        particle.show();
+        //particle.update();
+    }
 }
 
 // Flock object
@@ -34,10 +51,38 @@ class Flock {
         this.boids = []; // Initialize the array
     }
 
-    run() {
-        for (let i = 0; i < this.boids.length; i++) {
-            this.boids[i].run(this.boids); // Passing the entire list of boids to each boid individually
+    closest(pos) {
+        let bestBoid = createVector(mouseX, mouseY);
+        let bestDist = pos.dist(bestBoid);
+
+        for (let boid of this.boids) {
+            let d = pos.dist(boid.position);
+            if (d < bestDist) {
+                bestDist = d;
+                bestBoid = boid.position;
+            }
+            if (d < 10) {
+                boid.dead = true;
+                serpent.x.push(0);
+                serpent.y.push(0);
+                splatters.push(new Splatter(boid.position.x, boid.position.y));
+            }
         }
+        strokeWeight(1);
+        stroke(0, 50);
+        line(pos.x, pos.y, bestBoid.x, bestBoid.y);
+        return bestBoid;
+    }
+
+    run() {
+        this.next = [];
+        for (let boid of this.boids) {
+            boid.run(this.boids); // Passing the entire list of boids to each boid individually
+            if (boid.dead == false) {
+                this.next.push(boid);
+            }
+        }
+        this.boids = this.next;
     }
     addBoid(b) {
         this.boids.push(b);
@@ -59,6 +104,7 @@ class Boid {
         this.r = 3.0;
         this.maxspeed = 4; // Maximum speed
         this.maxforce = 0.05; // Maximum steering force
+        this.dead = false;
     }
 
     run(boids) {
@@ -84,6 +130,7 @@ class Boid {
         this.applyForce(chase);
         this.applyForce(sep);
     }
+
     // Separation
     // Method checks for nearby boids and steers away
     separate(boids) {
@@ -148,6 +195,7 @@ class Boid {
         let theta = this.velocity.heading() + radians(90);
         fill(127);
         stroke(0);
+        strokeWeight(1);
         push();
         translate(this.position.x, this.position.y);
         rotate(theta);
@@ -165,5 +213,92 @@ class Boid {
         if (this.position.y < -this.r) this.position.y = height + this.r;
         if (this.position.x > width + this.r) this.position.x = -this.r;
         if (this.position.y > height + this.r) this.position.y = -this.r;
+    }
+}
+
+class Serpent extends Boid {
+    constructor() {
+        super();
+        // this.acceleration = createVector(0, 0);
+        // this.velocity = createVector(random(-1, 1), random(-1, 1));
+        // this.position = createVector(x, y);
+        // this.r = 3.0;
+        // this.maxspeed = 4; // Maximum speed
+        // this.maxforce = 0.05; // Maximum steering force
+        this.maxspeed = 2;
+        this.maxforce = 0.1;
+        this.x = new Array(segNum).fill(0);
+        this.y = new Array(segNum).fill(0);
+    }
+
+    dragSegment(i, xin, yin) {
+        var dx = xin - this.x[i];
+        var dy = yin - this.y[i];
+        var angle = atan2(dy, dx);
+        this.x[i] = xin - cos(angle) * segLength;
+        this.y[i] = yin - sin(angle) * segLength;
+        this.segment(this.x[i], this.y[i], angle);
+    }
+
+    segment(x, y, a) {
+        push();
+        translate(x, y);
+        rotate(a);
+        line(0, 0, segLength, 0);
+        pop();
+    }
+
+    show() {
+        strokeWeight(8);
+        stroke(0);
+        this.dragSegment(0, this.position.x, this.position.y);
+        for (var i = 0; i < this.x.length - 1; i++) {
+            this.dragSegment(i + 1, this.x[i], this.y[i]);
+        }
+    }
+
+    chase(flock) {
+        let prey = flock.closest(serpent.position);
+        //let prey = createVector(mouseX, mouseY);
+        let aim = this.seek(prey);
+        this.applyForce(aim);
+        this.update();
+        this.borders();
+    }
+
+    // Wraparound
+    borders() {
+        if (this.position.x < 0) this.position.x = 0;
+        if (this.position.y < 0) this.position.y = 0;
+        if (this.position.x > width) this.position.x = width;
+        if (this.position.y > height) this.position.y = height;
+    }
+}
+
+class Splatter {
+    constructor(x, y) {
+        this.life = 1;
+        this.decay = 0.01;
+        this.x = x;
+        this.y = y;
+        this.particles = [];
+        for (let i = 0; i < 5; i++) {
+            let particle = createVector(x + random(10), y + random(10));
+            this.particles.push([particle, random(5)]);
+        }
+    }
+    update() {
+        if (this.life > 0) {
+            this.life -= this.decay;
+        }
+    }
+    show() {
+        for (let splatter of this.particles) {
+            let particle = splatter[0];
+            let size = splatter[1];
+            noStroke();
+            fill(255, 0, 0, 100 * this.life);
+            ellipse(particle.x, particle.y, size, size);
+        }
     }
 }
