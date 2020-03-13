3f592db8f7c725f66e1a825a5696b97a10fb48a8
lfrati <lfrati@uvm.edu>
2020-03-13 14:52:30 -0400

    Update the sketch on infection patterns, it really sucked.

--- _projects/Infection/2019-02-17-Infection.md
+++ _projects/Infection/2019-02-17-Infection.md
@@ -1,25 +1,42 @@
 ---
 layout: post
-title: 'Infectious edges.'
+title: 'Infectious Patterns.'
 date: 2019-02-17
 thumbnail: thumbnail.png
 dependencies:
 ---
 
-# Infection
-
 {% raw %}
-
 <script>
 function resizeIframe(obj) {
 obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
 }
 </script>
+{% endraw %}
+
+# Infections
+
+What happens when an infected little vector starts visiting nodes in an unsuspecting network? In the sketch below we can try to drop in some nasty vectors in an otherwise peaceful network and see what happens.
+
+{% raw %}
 <iframe frameborder="0" marginheight="20" marginwidth="35" scrolling="no" onload="resizeIframe(this)" src="p5/graph_infection/index.html"></iframe>
 {% endraw %}
 
-{% raw %}
+# Patterns
+Imagine now that we were sitting in one of those unsuspecting nodes, minding our own business. No internet, no cellphones, no telegraphs, just us sitting placidly on our chair reading our favourite book.
+If we were to take a look at the infected vectors that visit our little node what would we see? If the infection is spreading wildly we would likely see that their rate of arrival starts raising dramatically. Or maybe the infection is dying out and little by little fewer and fewer of them would visit our node.
 
+Imagine now that we have no idea of what the network of nodes we are connected to looks like, what would we be able to discover about it just from keeping track of the times at which new vectors visit our own node? Let us consider a few simple scenarios in the following sketch (click to reset it).
+
+{% raw %}
 <iframe frameborder="0" marginheight="20" marginwidth="35" scrolling="no" onload="resizeIframe(this)" src="p5/patterns/index.html"></iframe>
 {% endraw %}
+
+That looks complicated. Even a simple pair of loops create quite irregular patterns. The situation is even worse if we consider that every possible path through the networks leads to a different "rhythm". In our little example we have very simple, color coded paths but in general the number of possible paths grows exponentially and... well good luck.
+
+In order to make sense of this mess we are going to use some extra info. In our previous example signals are color coded based on the path they have followed but that's a bit like cheating isn't it. We cannot expect someone to go out and label all the paths in our network for us. But maybe we can get away with less detailed info, for example what about "age"?
+
+Let us pretend that the new signals we input first in our network have `age = 0` and that whenever signals split at a node they generate "clones" with the same age as the original. All that's left to do is to periodically update their age and now we can keep track of how long incoming signals have been in the network for.
+
+TODO
--- _projects/Infection/p5/patterns/pattern.js
+++ _projects/Infection/p5/patterns/pattern.js
@@ -1,3 +1,4 @@
+// @format
 let num_nodes = 50; // How many nodes
 let num_links = 100; // How many links
 let nodes = [];
@@ -9,289 +10,285 @@ let buffer = 50; // Leave some space around the network
 let ID = 0; // Used to map nodes to rows of the adjacency matrix
 let adj; // Adjacency matrix
 let node_diameter = 10; // How big should the nodes be?
-let slider;
+let slider_y, slider_x;
 let focused = true;
-
-function mousePressed() {
-    for (let node of nodes) {
-        let d = dist(mouseX, mouseY, node.pos.x, node.pos.y);
-        if (d < 10) {
-            node.infect(true);
-        }
-    }
-}
+let movable_node, movable_node_x, movable_node_y;
 
 let filter = 0;
-let rate = 5; // wheel events are REALLY fast, filter some of them
-function mouseWheel(event) {
-    //print(event.delta);
-    filter = (filter + 1) % rate;
-    if (filter == 0) {
-        for (let node of nodes) {
-            let d = dist(mouseX, mouseY, node.pos.x, node.pos.y);
-            if (d < 10) {
-                node.infect(true);
-            }
-        }
-    }
-
-    return false; // return false otherwise the page would scroll
-}
 
 function addNode(x, y) {
-    let pos = createVector(x, y);
-    let node = new Node(pos);
-    nodes.push(node);
-    return node;
+  let pos = createVector(x, y);
+  let node = new Node(pos);
+  nodes.push(node);
+  return node;
 }
-function connect(from, to) {
-    let link = new Link(from, to);
-    links.push(link);
-    from.out.push(link); // to infect neighbors
-    to.in.push(link); // mmmh... maybe we'll use it later
+function connect(from, to, col) {
+  let link = new Link(from, to);
+  if (col != undefined) {
+    link.col = col;
+  }
+  links.push(link);
+  from.out.push(link); // to infect neighbors
+  to.in.push(link); // mmmh... maybe we'll use it later
 }
 
 function pattern1() {
-    let head = addNode(20, 150);
-    let halfway = addNode(170, 60);
+  let head = addNode(20, 160);
+  head.color = "red";
+  let halfway = addNode(170, 40);
 
-    connect(
-        head,
-        halfway
-    );
+  connect(head, halfway, "yellow");
 
-    let tail = addNode(320, 150);
-    connect(
-        head,
-        tail
-    );
-    connect(
-        halfway,
-        tail
-    );
-    let readout = addNode(width - 20, 150);
-    connect(
-        tail,
-        readout
-    );
+  let tail = addNode(320, 160);
+  connect(head, tail);
+  connect(halfway, tail);
+  let readout = addNode(width - 20, 160);
+  connect(tail, readout);
+  readout.color = "grey";
 
-    let p1 = setInterval(() => {
-        head.infect(true);
-    }, 500);
-    return p1;
+  let p1 = setInterval(() => {
+    head.infect(true);
+  }, 300);
+  return p1;
 }
 
 function pattern2() {
-    let head = addNode(20, 350);
-    let halfway = addNode(170, 350);
-    connect(
-        head,
-        halfway
-    );
-    let loop1 = addNode(220, 250);
-    connect(
-        halfway,
-        loop1
-    );
-    let loop2 = addNode(120, 250);
-    connect(
-        loop1,
-        loop2
-    );
-    connect(
-        loop2,
-        halfway
-    );
-    let tail = addNode(320, 350);
-    connect(
-        halfway,
-        tail
-    );
-    let readout = addNode(width - 20, 350);
-    connect(
-        tail,
-        readout
-    );
+  let head = addNode(20, 360);
+  head.color = "red";
+  let halfway = addNode(170, 360);
+  connect(head, halfway);
+  let loop1 = addNode(230, 260);
+  connect(halfway, loop1, "yellow");
+  let loop2 = addNode(110, 260);
+  connect(loop1, loop2);
+  connect(loop2, halfway);
+  let readout = addNode(width - 20, 360);
+  connect(halfway, readout);
+  readout.color = "grey";
 
-    let p2 = setInterval(() => {
-        head.infect(true);
-    }, 500);
-    return p2;
+  let p2 = setInterval(() => {
+    head.infect(true);
+  }, 300);
+  return p2;
 }
 
-function setup() {
-    let cnv = createCanvas(800, 600);
-    cnv.mouseOver(() => {
-        focused = true;
-    });
-    cnv.mouseOut(() => {
-        focused = false;
-    });
+function pattern3() {
+  let head = addNode(20, 530);
+  head.color = "red";
+  let halfway = addNode(170, 610);
 
-    slider = createSlider(0, 100, 100, 1); // range[0-100] start from 55, increments of 5
-    slider.position(100, 5);
-    slider.style('width', '100px');
-    slider.mouseOver(() => {
-        focused = true;
-    });
-    slider.mouseOut(() => {
-        focused = false;
-    });
+  let loop1 = addNode(220, 690);
+  connect(halfway, loop1, "violet");
+  connect(head, halfway, "yellow");
+
+  let loop2 = addNode(120, 690);
+  connect(loop1, loop2);
+  connect(loop2, halfway);
+
+  let loop4 = addNode(170, 530);
+  movable_node = loop4;
+  movable_node.color = "white";
+  movable_node_x = loop4.pos.x;
+  movable_node_y = loop4.pos.y;
+
+  let loop5 = addNode(250, 460);
+  let loop6 = addNode(100, 460);
+  connect(loop4, loop5, "blue");
+  connect(loop5, loop6);
+  connect(loop6, loop4);
+  connect(head, loop4);
+
+  let tail = addNode(320, 530);
+  connect(halfway, tail);
+  connect(loop4, tail);
+
+  let readout = addNode(width - 20, 530);
+  connect(tail, readout);
+  readout.color = "grey";
+
+  let p3 = setInterval(() => {
+    head.infect(true);
+  }, 300);
+  return p3;
+}
+
+let uncovered = true;
+function setup() {
+  let cnv = createCanvas(900, 740);
+
+  cnv.mouseMoved(() => {
+    if (mouseX < width && mouseX > 300 && mouseY > 0 && mouseY < height) {
+      uncovered = true;
+    } else {
+      uncovered = false;
+    }
+  });
+
+  cnv.mouseClicked(() => {
     makePatterns();
+  });
+
+  slider_y = createSlider(-150, 150, 0, 10); // range[0-100] start from 55, increments of 5
+  slider_x = createSlider(-150, 150, 0, 10); // range[0-100] start from 55, increments of 5
+  slider_y.position(190, 610);
+  slider_y.style("width", "100px");
+  slider_x.position(20, 610);
+  slider_x.style("width", "100px");
+  slider_x.style("rotate", "90");
+  makePatterns();
+  frameRate(30);
 }
 
 function makePatterns() {
-    nodes = [];
-    links = [];
-    cur_infected = 0;
+  nodes = [];
+  links = [];
+  cur_infected = 0;
 
-    let p1 = pattern1();
-    let p2 = pattern2();
+  let p1 = pattern1();
+  let p2 = pattern2();
+  let p3 = pattern3();
 
-    setTimeout(() => {
-        clearInterval(p1);
-        clearInterval(p2);
-    }, 2000);
+  setTimeout(() => {
+    clearInterval(p1);
+    clearInterval(p2);
+    clearInterval(p3);
+  }, 1000);
 
-    setTimeout(makePatterns, 10000);
+  // setTimeout(makePatterns, 10000);
+}
+
+function round10(x) {
+  return Math.ceil(x / 10) * 10;
 }
 
 function draw() {
-    if (focused) {
-        background(50);
+  background(50);
 
-        // Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
-        let fps = frameRate();
-        fill(255);
-        stroke(0);
-        textSize(15);
-        text('FPS: ' + fps.toFixed(0), width - 100, 20);
+  // Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
+  let fps = frameRate();
+  fill(255);
+  stroke(0);
+  textSize(15);
+  text("FPS: " + round10(fps), width - 100, 20);
 
-        spread_prob = slider.value() / 100;
-        stroke(0);
-        strokeWeight(2);
-        fill(220, 220, 220);
-        textSize(20);
-        text('P : ' + nf(spread_prob, 1, 2), 20, 20);
-        text('Infected : ' + cur_infected, 210, 20);
+  movable_node.pos.x = movable_node_x + slider_y.value();
+  movable_node.pos.y = movable_node_y + slider_x.value();
 
-        for (let link of links) {
-            link.show();
-            link.update();
-        }
+  for (let link of links) {
+    link.show();
+    link.update();
+  }
 
-        for (let node of nodes) {
-            node.show();
-        }
-    }
+  for (let node of nodes) {
+    node.show();
+  }
+
+  if (uncovered) {
+    fill(20);
+    strokeWeight(2);
+    stroke(0);
+    rect(30, 30, 275, height - 60);
+    fill(200);
+    textSize(40);
+    text("? ? ?", 120, height / 2);
+  }
 }
 
 class Node {
-    constructor(pos) {
-        this.pos = pos;
-        this.id = ID;
-        ID += 1;
-        this.out = [];
-        this.in = [];
-        this.has_been_infected = false;
-    }
-    // force is used so that clicking bypasses the infection probability
-    infect(force) {
-        if (force || random() < spread_prob) {
-            this.has_been_infected = true;
-            for (let edge of this.out) {
-                // if we have not reached the max amount of carriers add a new one
-                if (cur_infected < max_infected) {
-                    edge.infect();
-                    cur_infected += 1;
-                }
-            }
-        }
-    }
-
-    show() {
-        noStroke(); // no border
-        if (this.out.length == 0) {
-            // not outgoing nodes
-            if (this.in.length == 0) {
-                // disconnected -> blue
-                fill(50, 50, 250);
-            } else {
-                // dead end -> purple
-                fill(160, 50, 250);
-            }
+  constructor(pos) {
+    this.pos = pos;
+    this.id = ID;
+    ID += 1;
+    this.out = [];
+    this.in = [];
+    this.has_been_infected = false;
+    this.color = "green";
+  }
+  // force is used so that clicking bypasses the infection probability
+  infect(force, col) {
+    for (let edge of this.out) {
+      // if we have not reached the max amount of carriers add a new one
+      if (cur_infected < max_infected) {
+        if (force) {
+          edge.infect("red");
         } else {
-            if (this.has_been_infected) {
-                // a sickening yellow tint
-                fill(255, 255, 70);
-            } else {
-                // a healthy greenish color
-                fill(0, 150, 10);
-            }
+          edge.infect(col);
         }
-        ellipse(this.pos.x, this.pos.y, node_diameter, node_diameter);
+        cur_infected += 1;
+      }
     }
+  }
+
+  show() {
+    noStroke(); // no border
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, node_diameter, node_diameter);
+  }
 }
 
 // to show the direction of the link, require some calculation but it's worth it.
 function arrowhead(from, to, base, height, distance) {
-    let rot = p5.Vector.sub(to.pos, from.pos).heading(); // get angle of the link
-    push();
-    translate(to.pos.x, to.pos.y); // move the origin to the target the arrow is pointing to
-    rotate(rot); // rotate to align the tip
-    noStroke(); // strong independent arrows need no border
-    fill(255, 50); // a bit of transparency, we want to see them if they overlap
-    triangle(-distance / 2 - 1, 0, -distance / 2 - height, -base, -distance / 2 - height, +base);
-    pop();
+  let rot = p5.Vector.sub(to.pos, from.pos).heading(); // get angle of the link
+  push();
+  translate(to.pos.x, to.pos.y); // move the origin to the target the arrow is pointing to
+  rotate(rot); // rotate to align the tip
+  noStroke(); // strong independent arrows need no border
+  fill(255, 50); // a bit of transparency, we want to see them if they overlap
+  triangle(-distance / 2 - 1, 0, -distance / 2 - height, -base, -distance / 2 - height, +base);
+  pop();
 }
 
 class Link {
-    constructor(from, to) {
-        this.from = from;
-        this.to = to;
-        this.carriers = [];
-        this.dt = 3; // step length along the link, longer = faster
-        this.length = p5.Vector.dist(this.from.pos, this.to.pos);
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+    this.carriers = [];
+    this.dt = 3; // step length along the link, longer = faster
+    this.length = p5.Vector.dist(this.from.pos, this.to.pos);
+    this.col = undefined;
+  }
+
+  infect(col) {
+    if (this.col == undefined) {
+      this.carriers.push([col, 0]);
+    } else {
+      this.carriers.push([this.col, 0]);
+    }
+  }
+
+  show() {
+    strokeWeight(1);
+    // paint link red if there are carriers travelling on it
+    if (this.carriers.length == 0) {
+      stroke(255, 50); // default is greyish
+    } else {
+      stroke(255, 50, 50); // infected is reddish
     }
 
-    infect() {
-        this.carriers.push(0);
+    line(this.from.pos.x, this.from.pos.y, this.to.pos.x, this.to.pos.y);
+    arrowhead(this.from, this.to, 4, 10, node_diameter);
+    for (let [col, progress] of this.carriers) {
+      let tmp = p5.Vector.lerp(this.from.pos, this.to.pos, progress / p5.Vector.dist(this.from.pos, this.to.pos));
+      noStroke();
+      fill(col);
+      ellipse(tmp.x, tmp.y, 7, 7);
     }
+  }
 
-    show() {
-        strokeWeight(1);
-        // paint link red if there are carriers travelling on it
-        if (this.carriers.length == 0) {
-            stroke(255, 50); // default is greyish
-        } else {
-            stroke(255, 50, 50); // infected is reddish
-        }
+  update() {
+    let new_carriers = [];
 
-        line(this.from.pos.x, this.from.pos.y, this.to.pos.x, this.to.pos.y);
-        arrowhead(this.from, this.to, 4, 10, node_diameter);
-
-        for (let carrier of this.carriers) {
-            let tmp = p5.Vector.lerp(this.from.pos, this.to.pos, carrier / this.length);
-            noStroke();
-            fill(255);
-            ellipse(tmp.x, tmp.y, 5, 5);
-        }
+    for (let [col, progress] of this.carriers) {
+      // move
+      let new_progress = progress + this.dt;
+      if (new_progress < p5.Vector.dist(this.from.pos, this.to.pos)) {
+        new_carriers.push([col, new_progress]);
+      } else {
+        // destination reached
+        this.to.infect(false, col);
+        cur_infected -= 1;
+      }
     }
-
-    update() {
-        let new_carriers = [];
-
-        for (let carrier of this.carriers) {
-            // move
-            let new_traveler = carrier + this.dt;
-            if (new_traveler < this.length) {
-                new_carriers.push(new_traveler);
-            } else {
-                // destination reached
-                this.to.infect(false);
-                cur_infected -= 1;
-            }
-        }
-        this.carriers = new_carriers;
-    }
+    this.carriers = new_carriers;
+  }
 }
